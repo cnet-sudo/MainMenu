5b7684a7ff379ddb023b9e1c42eaf3a3b2b63411
cnet-sudo <cnetcnetcnet3@gmail.com>
2022-12-29 23:29:50 +0200

    Добавление звука

--- GameMenu.h
+++ GameMenu.h
@@ -1,7 +1,5 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-//#include <iostream>
-
 
 namespace game {
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include"GameMenu.h"
+#include<SFML/Audio.hpp>
 
 using namespace sf;
 // функция настройки текста
@@ -16,12 +17,13 @@ void InitText(Text & mtext,float xpos, float ypos,String str, int size_font=60, 
 // Игра
 void GamеStart()
 {
-    RenderWindow Play(VideoMode(1280, 720), L"Уровень 1", Style::Fullscreen);
+    RenderWindow Play(VideoMode::getDesktopMode(), L"Уровень 1", Style::Fullscreen);
+      
+    
+    RectangleShape background_play(Vector2f(1920, 1080));
 
-    RectangleShape background_play(Vector2f(1280, 720));
     Texture texture_play;
     if (!texture_play.loadFromFile("image/menu4.jpg")) exit(1);
-   
     background_play.setTexture(&texture_play);
 
     while (Play.isOpen())
@@ -29,10 +31,9 @@ void GamеStart()
         Event event_play;
         while (Play.pollEvent(event_play))
         {
-            if (event_play.type == Event::Closed) Play.close();
             if (event_play.type == Event::KeyPressed)
             {
-                if (event_play.key.code == Keyboard::Escape) Play.close();
+                if (event_play.key.code == Keyboard::Escape) { Play.close();}
             }
         }
         Play.clear();
@@ -43,9 +44,9 @@ void GamеStart()
 // Настройки игры
 void Options() 
 {
-    RenderWindow Options(VideoMode(1280, 720), L"Настройки", Style::Fullscreen);
+    RenderWindow Options(VideoMode::getDesktopMode(), L"Настройки", Style::Fullscreen);
 
-    RectangleShape background_opt(Vector2f(1280, 720));
+    RectangleShape background_opt(Vector2f(1920, 1080));
     Texture texture_opt;
     if (!texture_opt.loadFromFile("image/menu1.jpg")) exit(2);
     
@@ -70,13 +71,20 @@ void Options()
 // Описание игры
 void About_Game() 
 {
-    RenderWindow About(VideoMode(1280, 720), L"О игре", Style::Fullscreen);
+    RenderWindow About(VideoMode::getDesktopMode(), L"О игре", Style::Fullscreen);
 
-    RectangleShape background_ab(Vector2f(1280, 720));
+    RectangleShape background_ab(Vector2f(VideoMode::getDesktopMode().width, VideoMode::getDesktopMode().height));
     Texture texture_ab;
     if (!texture_ab.loadFromFile("image/menu2.jpg")) exit(3);
-    
     background_ab.setTexture(&texture_ab);
+
+    // Шрифт для названия экрана
+    Font font;
+    if (!font.loadFromFile("font/troika.otf")) exit(6);
+    // Текст с названием экрана
+    Text Titul;
+    Titul.setFont(font);
+    InitText(Titul, 500, 50, L"Описание игры", 120, Color(237, 147, 0), 3);
 
     while (About.isOpen())
     {
@@ -91,40 +99,63 @@ void About_Game()
         }
         About.clear();
         About.draw(background_ab);
+        About.draw(Titul);
         About.display();
     }
 }
 
 int main()
 {
+   
+    RenderWindow window(VideoMode::getDesktopMode(), L"Моя игра", Style::Fullscreen);
+    window.setVerticalSyncEnabled(true);
+    window.setMouseCursorVisible(false); //отключаем видимость курсора
+
+    SoundBuffer buffer,buf_return;
     
-    RenderWindow window(VideoMode(1280, 720), L"Моя игра", Style::Fullscreen);
-    window.setMouseCursorVisible(false);
+    if (!buffer.loadFromFile("audio/audiomenu2.wav")) return 22;
+    if (!buf_return.loadFromFile("audio/audiomenu5.wav")) return 22;
+    Sound sound,sound_return;
+    sound.setBuffer(buffer);
+    sound_return.setBuffer(buf_return);
+
+    Music music;
+    if (!music.openFromFile("audio/Anxiety.ogg")) return 25; 
+    music.setLoop(true);
+    music.play();
+   
+
     // Координаты выравнивания текста
-    float center[4]{ 0,-70,-10,-15 };
+    float center[4]{ 0,-120,-20,-30 };
     // Название пунктов меню
     String name_menu[4]{ L"Старт",L"Настройки", L"О игре",L"Выход" };
     // Объект меню
-    game::GameMenu mymenu(550, 250);
+    game::GameMenu mymenu(800,350,100,120);
     // Установка цвета отображения меню
     mymenu.setColotTextMenu(Color(237, 147, 0), Color::Red, Color::Black);
-
+    // Переименовываем пункты меню
     for (int i = 0; i < 4; i++) { 
         mymenu.setPositionX(i, center[i]);
-        mymenu.setStringMenu(i, name_menu[i]); }
+        mymenu.setStringMenu(i, name_menu[i]); 
+    }
 
-    
-    RectangleShape background(Vector2f(1280, 720));
+
+    // Устанавливаем фон экрана меню
+    float width = VideoMode::getDesktopMode().width;
+    float height = VideoMode::getDesktopMode().height;
+
+    RectangleShape background(Vector2f(1920, 1080));
     Texture texture_window;
     if (!texture_window.loadFromFile("image/menu9.jpg")) return 4;
     background.setTexture(&texture_window);
 
-    Text Titul;
+    // Шрифт для названия экрана
     Font font;
     if (!font.loadFromFile("font/troika.otf")) return 5;
-   
+    // Текст с названием экрана
+    Text Titul;
     Titul.setFont(font);
-    InitText(Titul,320,50,L"Апокалипсис",100, Color(237,147,0),3);
+    InitText(Titul,480,50,L"Апокалипсис",150, Color(237,147,0),3);
 
 
     while (window.isOpen())
@@ -132,32 +163,36 @@ int main()
         Event event;
         while (window.pollEvent(event))
         {
-            switch (event.type)
-            {
-            case Event::Closed:
-                window.close(); break;
-            case Event::KeyReleased:
-                if (event.key.code == Keyboard::Up) { mymenu.MoveUp(); break; }
-                if (event.key.code == Keyboard::Down) { mymenu.MoveDown(); break;}
-                if (event.key.code == Keyboard::Return)
-                {                  
+            
+              if(event.type==Event::Closed) window.close();
+                
+              if (event.type==Event::KeyReleased) 
+              {
+                // События выбра пунктов меню
+                  if (event.key.code == Keyboard::Up) { sound.play(); mymenu.MoveUp(); }       // вверх
+                  if (event.key.code == Keyboard::Down) { sound.play(); mymenu.MoveDown(); }  // вниз
+                if (event.key.code == Keyboard::Return)                    // ввод
+                {   
+                    music.pause();
+                    sound_return.play();
+                    // Переходим на выбранный пункт меню
                     switch (mymenu.getMainMenuPressed())
                     {
-                    case 0:GamеStart(); break;
-                    case 1:Options(); break;
-                    case 2:About_Game(); break;
+                    case 0:GamеStart(); music.play(); break;
+                    case 1:Options(); music.play(); break;
+                    case 2:About_Game(); music.play(); break;
                     case 3:window.close(); break;
                    
                     }  
-                } break;
-            }
+                } 
+              }
         }
         
        
-        window.clear(Color::Blue);
+        window.clear();
         window.draw(background);
         window.draw(Titul);
-        mymenu.draw(window);        
+        mymenu.draw(window);  
         window.display();
     }
     return 0;
